9e685e8d0a2188230cedac60c98a75332cf46855
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-28 19:21:08 -0500

    wrote dockerfile, build scripts, apache conf

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Prepare a FQDN into a domain name.
+# On Linux, dnsdomainname can be used,
+# but using cut allows for backwards-compatibility
+# with Mac OS.
+production_domain=environmentaldashboard.org
+domain=`cut -f 2- -d . <<< $HOSTNAME`
+
+if [ "$domain" = "$production_domain" ] || [ "$HOSTNAME" = "$production_domain" ]
+then
+	# prod env:
+	docker run -dit -p 5000:80 --restart always --name PROD_DS digital-signage
+else
+	# dev env:
+	# (bind mounting code so changes to code don't require image rebuild)
+	docker run -dit -p 5000:80 --restart always -v $(pwd):/var/www/html/ --name DEV_DS digital-signage
+fi
