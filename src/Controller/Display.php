edab320a469fa7209b844f42c31c695d4af80a9b
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-03-25 09:17:25 -0500

    more work on present method of Display controller

--- src/Controller/Display.php
+++ src/Controller/Display.php
@@ -26,13 +26,23 @@ class Display extends AbstractController
         $displayRepository = $entityManager->getRepository(Entity\Display::class);
         $display = $displayRepository->find($id);
 
-        foreach ($display->getPresentations() as $i => $presentation) {
-            $template = $presentation->getTemplate();
+        foreach ($display->getPresentations() as $presentation) {
+            $twig = $presentation->getTemplate()->getTwig();
+            $template = $this->get('twig')->createTemplate($twig);
+            $pres_carousels = [];
+
             $map = $presentation->getCarouselPresentationMaps();
-            $cycle[$i] = ['template' => $template, 'carousels' => []];
             foreach ($map as $relation) {
-                $cycle[$i]['carousels'][] = $relation->getCarousel();
+                $key = $relation->getTemplateKey();
+                $carousel = $relation->getCarousel();
+                $iframes = [];
+                foreach ($carousel->getFrames() as $frame) {
+                    $iframes[] = "<iframe src='{$frame->getUrl()}' data-duration='{$frame->getDuration()}' frameborder='0' style='width: 100%;height: 100%;'></iframe>";
+                }
+                $pres_carousels[$key] = implode('', $iframes);
             }
+            $rendered = $template->render($pres_carousels);
+            $cycle[] = $rendered;
         }
 
         return $this->render('present.html.twig', ['cycle' => $cycle]);
