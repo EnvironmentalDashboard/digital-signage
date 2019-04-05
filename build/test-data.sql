6ae7ce16a014c2a4b83dc07aa20f370521d91a95
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-04-04 23:47:17 -0400

    better test data

--- build/test-data.sql
+++ build/test-data.sql
@@ -1,13 +1,18 @@
-INSERT INTO "frame" ("id","carousel_id","url","duration") VALUES (3,1,'https://news.ycombinator.com',10),
- (4,2,'https://css-tricks.com',10),
- (5,3,'http://example.com',10),
- (6,1,'http://www.espn.com',10);
-INSERT INTO "presentation" ("id","template_id","display_id","label","skip","duration") VALUES (3,2,1,'pres 1',0,10),
- (4,1,1,'pres 2',0,10);
-INSERT INTO "carousel_presentation_map" ("id","presentation_id","carousel_id","template_key") VALUES (5,3,1,'url1'),
- (6,3,2,'url2'),
- (7,4,3,'url1');
+INSERT INTO "frame" ("id","carousel_id","url","duration") VALUES (1,1,'https://sewanee.environmentaldashboard.org/calendar/',4000),
+ (2,2,'https://obp.environmentaldashboard.org/calendar/',4000),
+ (3,3,'https://environmentaldashboard.org',4000),
+ (4,1,'https://environmentaldashboard.org/cleveland/chart/?meter0=2813',4000),
+ (5,1,'https://environmentaldashboard.org/community-voices/public/digital-signage.php',4000),
+ (6,2,'https://oberlin.environmentaldashboard.org/calendar/',4000);
+INSERT INTO "presentation" ("id","template_id","display_id","label","skip","duration") VALUES (1,2,1,'marquee pres',0,25000),
+ (2,1,1,'fullscreen pres',0,25000);
+INSERT INTO "carousel_presentation_map" ("id","presentation_id","carousel_id","template_key") VALUES (1,2,1,'url1'),
+ (2,1,3,'url1'),
+ (3,1,2,'url2');
 INSERT INTO "carousel" ("id","label") VALUES (1,'carousel 1'),
  (2,'carousel 2'),
  (3,'carousel 3');
 INSERT INTO "display" ("id","label") VALUES (1,'display 1');
+INSERT INTO "remote_controller" ("id","label","template_id") VALUES (1,"test controller", 0);
+INSERT INTO "button" ("id","controller_id","trigger_frame_id","on_display_id","twig_key") VALUES (1, 1, 5, 1, "btn1"),
+	(2, 1, 4, 1, "btn2");
