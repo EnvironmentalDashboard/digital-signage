7240af9bbc8196f5fda6d6298e8c1047bb88eec8
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-04-21 16:14:45 -0400

    new doctrine GoogleSlides entity

---
+++ src/Entity/GoogleSlides.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GoogleSlidesRepository")
+ */
+class GoogleSlides
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=44)
+     */
+    private $presentationId;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $data = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPresentationId(): ?string
+    {
+        return $this->presentationId;
+    }
+
+    public function setPresentationId(string $presentationId): self
+    {
+        $this->presentationId = $presentationId;
+
+        return $this;
+    }
+
+    public function getData(): ?array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
