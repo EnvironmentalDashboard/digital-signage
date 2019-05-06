7a750ad71fc444a69b37089ccc697cb6fda281bc
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-05-06 14:10:31 -0400

    need to install mysql in init.sh, updated readme

--- build/init.sh
+++ build/init.sh
@@ -2,7 +2,7 @@
 
 # install apt packages, set timezone (see: https://serverfault.com/a/683651/456938), install composer
 apt-get update
-apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml curl php-curl sqlite php-sqlite3 php-xdebug php-gd unzip
+apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml curl php-curl php-xdebug php-gd unzip sqlite php-sqlite3 php-mysql
 
 INI_LOC=`php -i | grep 'Loaded Configuration File => ' | sed 's/Loaded Configuration File => //g' | sed 's/cli/apache2/g'`
 sed -ie 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' "$INI_LOC"
@@ -17,5 +17,3 @@ php composer.phar update
 phpdismod xdebug
 a2enmod rewrite headers
 mv /var/www/html/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
-# https://symfony.com/doc/master/bundles/DoctrineMigrationsBundle/index.html
-php ./bin/console doctrine:database:create && php ./bin/console doctrine:migrations:diff && php ./bin/console doctrine:migrations:migrate
