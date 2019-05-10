0c3374bf3d0e4c276a877d5280d1d432ca49ef78
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-05-10 12:52:18 -0400

    fixed that annoying symfony app_env bug

--- build/default-templates.sql
+++ build/default-templates.sql
@@ -1,2 +1,3 @@
-INSERT INTO "template" ("id","twig") VALUES (1,'<div class="carousel" data-twig="url1" style="height:100%;width:100%;position:relative">{{ url1|raw }}</div> '),
- (2,'<div class="carousel" data-twig="url1" style="height:80%;width:100%;position:relative">{{ url1|raw }}</div><div class="carousel" data-twig="url2" style="height:20%;width:100%;position:relative">{{ url2|raw }}</div>');
+INSERT INTO `template` (`id`, `twig`) VALUES
+(1, '<div class="carousel" data-twig="url1" style="height:100%;width:100%;position:relative">{{ url1|raw }}</div> '),
+(2, '<div class="carousel" data-twig="url1" style="height:80%;width:100%;position:relative">{{ url1|raw }}</div><div class="carousel" data-twig="url2" style="height:20%;width:100%;position:relative">{{ url2|raw }}</div> ');
--- build/init.sh
+++ build/init.sh
@@ -14,6 +14,8 @@ echo $TZ > /etc/timezone
 ./build/composer-install.sh
 php composer.phar install
 php composer.phar update
+php composer.phar dump-env prod
 phpdismod xdebug
 a2enmod rewrite headers
 mv /var/www/html/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
+chown -R www-data:www-data /var/www/html
