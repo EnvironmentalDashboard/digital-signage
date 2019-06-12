28be678600a4425d1e72325855b34115b6eab8c3
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-06-12 15:57:02 -0400

    some js updates to remote controllers

--- templates/js/commandReceiver.js
+++ templates/js/commandReceiver.js
@@ -1,17 +1,23 @@
 // open websocket conn to recieve commands from remote controllers
-var conn = new WebSocket("ws://localhost:5001/display/{{ app.request.get('id') }}");
+/*{% set wsPort = app.request.port + 1 %}*/
+var conn = new WebSocket("ws://{{app.request.host}}:{{wsPort}}/display/{{ app.request.get('id') }}");
 conn.onmessage = function (e) {
 	var frame_id = parseInt(e.data);
-	if (e.origin !== 'ws://localhost:5001' || frame_id < 1) {
+	if (e.origin !== 'ws://{{app.request.host}}:{{wsPort}}' || frame_id < 1) {
 		return;
 	}
 	var frame = document.getElementById('frame' + frame_id);
+	if (frame === null) {
+		console.log(e.data);
+		return;
+	}
 	var carousel = frame.parentNode;
 	var pres = carousel.parentNode;
 	var all_frames = carousel.children;
 	var i;
 	for (i = 0; i < all_frames.length; i++) {
 		all_frames[i].style.display = 'none';
+		all_frames[i].className = 'none';
 	}
 	frame.style.display = 'initial';
 	if (pres.style.display === 'none') { // not already active pres
--- templates/js/remoteController.js
+++ templates/js/remoteController.js
@@ -1,13 +1,11 @@
-{% set wsPort = app.request.port + 1 %}
+/*{% set wsPort = app.request.port + 1 %}*/
 var conn = new WebSocket('ws://{{app.request.host}}:{{wsPort}}/remote-controller/{{controllerId}}');
 	conn.onmessage = function(e) { console.log(e.data); };
-	conn.onopen = function(e) { conn.send('1') };
 var clicked = function() {
 	var button_id = this.getAttribute("data-id");
-	console.log(button_id);
 	conn.send(button_id);
 }
 var buttons = document.getElementsByTagName("IMG");
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', clicked, false);
+	buttons[i].addEventListener('click', clicked, false);
 }
