8b831800b3e811efd9529787f845a57ae08faa52
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-06-16 13:04:55 -0400

    typo

--- run.sh
+++ run.sh
@@ -11,10 +11,10 @@ if [ "$domain" = "$production_domain" ] || [ "$HOSTNAME" = "$production_domain" 
 then
 	# prod env:
 	docker run -dit -p 5000:80 --restart always -v /var/www/uploads/digital-signage:/var/www/html/public/uploads -e APP_ENV=prod -e APP_DEBUG=0 --name PROD_DS digital-signage
-	docker run -dit -p 5001:80 --restart always -e APP_ENV=prod -e APP_DEBUG=0 --name PROD_DS_WS digital-signage-websockets
+	docker run -dit -p 5001:80 --restart always -e APP_ENV=prod -e APP_DEBUG=0 --name PROD_DS_WS digital-signage-wsserver
 else
 	# dev env:
 	# (bind mount code so changes to code don't require image rebuild, bind mount sqlite db so data persists across rebuilds)
-	docker run -dit -p 5000:80 --restart always -v $(pwd)/public/uploads:/var/www/html/public/uploads -v $(pwd)/var:/var/www/html/var/ -v $(pwd):/var/www/html/ -e APP_ENV=dev --name DEV_DS digital-signage-wsserver
+	docker run -dit -p 5000:80 --restart always -v $(pwd)/public/uploads:/var/www/html/public/uploads -v $(pwd)/var:/var/www/html/var/ -v $(pwd):/var/www/html/ -e APP_ENV=dev --name DEV_DS digital-signage
 	docker run -dit -p 5001:80 --restart always -v $(pwd):/var/www/html/ -e APP_ENV=dev --name DEV_DS_WS digital-signage-wsserver
 fi
