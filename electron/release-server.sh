381723c7af449f663e0999e506065905e7f1c352
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-17 12:08:08 -0400

    oops

--- electron/release-server.sh
+++ electron/release-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -f ./release-server.Dockerfile -t release-server . && \
-docker run -dit --name release-server -v ./dist:/usr/local/apache2/htdocs/ -p 1993:80 release-server
+docker run -dit --name release-server -v $(pwd)/dist:/usr/local/apache2/htdocs/ -p 1993:80 release-server
